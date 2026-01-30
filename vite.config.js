86b527c2e17b61c3c0a2d2b9500ca3502db04af8
Shayan Taghipour <shayan.taghipour86@gmail.com>
2026-01-30 21:00:43 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    base: '/',
     host: "0.0.0.0",
     fs: {
       strict: false,
